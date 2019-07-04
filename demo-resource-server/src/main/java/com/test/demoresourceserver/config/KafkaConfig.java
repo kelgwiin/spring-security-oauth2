ac550efcfb1749330ecbd0cf4d9fec6f6e7e1bb6
Kelwin Gamez <kelgwiin@gmail.com>
2019-07-04 11:02:35 -0500

    prod config

--- demo-resource-server/src/main/java/com/test/demoresourceserver/config/KafkaConfig.java
+++ demo-resource-server/src/main/java/com/test/demoresourceserver/config/KafkaConfig.java
@@ -1,19 +1,18 @@
 package com.test.demoresourceserver.config;
 
 import com.test.demoresourceserver.model.Customer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaConfig {
@@ -39,7 +38,6 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, Customer> kafkaTemplate() {
         KafkaTemplate<String, Customer> kafkaTemplate = new KafkaTemplate<>(producerFactory());
-//        kafkaTemplate.setMessageConverter(new StringJsonMessageConverter());
         return kafkaTemplate;
 
     }
